9a060598809679826625a7b24b8113e1be185ef5
Josh Jameson <josh@servebyte.com>
2020-01-09 21:02:52 +0000

    Added logout function

--- pwprotect.php
+++ pwprotect.php
@@ -38,4 +38,21 @@ EOF;
 exit;
   }
 }
+if (isset($_GET['logout'])) {
+  $_SESSION = array();
+  if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+              $params["path"], $params["domain"],
+              $params["secure"], $params["httponly"]
+             );
+  }
+  session_destroy();
+  $url = urldecode($_GET['logout']);
+  if (empty($url))
+    $url = substr($_SERVER['SCRIPT_URL'], 0, -strlen(basename($_SERVER['SCRIPT_URL'])));
+  header('Location: '.$url);
+  echo "<a href=\"$url\">$url</a>";
+  exit;
+}
 require ".$uri";
